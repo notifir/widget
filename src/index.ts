822859cad1d7de4b6187cd15b088e3fdecf90ebc
Katya Kuzmenchuk <kuzmenchukkatya@gmail.com>
2022-07-11 18:17:41 +0400

    refactor: improve styling

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,8 @@ interface SubscriptionData {
 }
 
 interface Stylesheet {
+  bell: StyleInfo
+  bellCounter: StyleInfo
   container: StyleInfo
   header: StyleInfo
   headerLink: StyleInfo
@@ -164,11 +166,14 @@ export class NotificationBell extends ApolloQuery {
 
     return html`
       <div>
-        <div class="bell" @click="${this._handleBellClick}">
+        <div class="bell" style=${styleMap(styles.bell || {})} @click="${this._handleBellClick}">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
             <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
           </svg>
-          ${unreadCount > 0 ? html`<div class="bell-counter">${unreadCount}</div>` : ''}
+          ${unreadCount > 0
+            ? html`<div class="bell-counter" style=${styleMap(styles.bellCounter || {})}>${unreadCount}</div>`
+            : nothing
+          }
         </div>
 
         <div class="popup">
